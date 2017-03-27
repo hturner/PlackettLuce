17ab80e6d59f847fb3ddfcddba01ba9b3be18684
Heather Turner <ht@heatherturner.net>
2017-03-27 16:22:09 +0100

    add DESCRIPTION and pre-commit hook

---
+++ pre-commit.R
@@ -0,0 +1,45 @@
+#' License: CC0 (just be nice and point others to where you got this)
+#' Author: Heather Turner <ht@heatherturner.net>, github.com/hturner
+#' Author: Robert M Flight <rflight79@gmail.com>, github.com/rmflight
+#' Author: Seth S Wenchel <wenchel@gmail.com>, github.com/restonslacker
+#'
+#' This is an R script associated with a pre-commit hook that checks whether
+#' there are files to be committed and if there are, will increment the package
+#' version and set today's date in the DESCRIPTION file.
+#'
+#' To install it, simply copy this into the top level directory of your git
+#' repository. Then, edit the ".git/hooks/pre-commit" file of your git repo to
+#' be
+#'
+#'   #!/bin/bash
+#'   Rscript pre-commit.R
+#'
+#' On Windows, the path to Rscript (which is the same as that to R itself)
+#' should be in the PATH system environment variable.
+#'
+
+# define path to DESCRIPTION
+DESCRIPTION <- "DESCRIPTION"
+
+# check that there are files that will be committed
+# - don't want to increment version if there won't be a commit
+fileDiff <- system("git diff HEAD --name-only", intern=TRUE)
+
+if ((length(fileDiff) > 0) && doIncrement){
+    currDCF <- read.dcf(DESCRIPTION)
+    currVersion <- currDCF[1,"Version"]
+    splitVersion <- strsplit(currVersion, ".", fixed=TRUE)[[1]]
+    nVer <- length(splitVersion)
+    currEndVersion <- as.integer(splitVersion[nVer])
+    newEndVersion <- as.character(currEndVersion + 1)
+    splitVersion[nVer] <- newEndVersion
+    newVersion <- paste(splitVersion, collapse=".")
+    currDCF[1,"Version"] <- newVersion
+    if (any(colnames(currDCF) == "Date")){
+        currDCF[1, "Date"] <- strftime(as.POSIXlt(Sys.Date()), "%Y-%m-%d")
+    } else currDCF <- cbind(currDCF,
+                            Date = strftime(as.POSIXlt(Sys.Date()), "%Y-%m-%d"))
+    write.dcf(currDCF, DESCRIPTION)
+    system(paste("git add", DESCRIPTION))
+    cat("Incremented package version and added to commit!\n")
+}
