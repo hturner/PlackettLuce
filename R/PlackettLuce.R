911c4048d8dc21b07001cb32c844d84863db084d
Heather Turner <ht@heatherturner.net>
2017-04-21 18:39:15 +0100

    eliminate theta

--- R/PlackettLuce.R
+++ R/PlackettLuce.R
@@ -68,17 +68,15 @@ PlackettLuce <- function(rankings, maxit = 100, trace = FALSE){
 
     ## starting values
     alpha <- rep.int(1/N, N)
-    #delta <- rep.int(0.1, D)
-    #delta[1] <- 1
-    delta <- c(1, (2*B[2])/(sum(rep) - B[2]))
-    theta <- rep.int(1, S)
+    delta <- rep.int(0.1, D)
+    delta[1] <- 1
+    #delta <- c(1, (2*B[2])/(sum(rep) - B[2]))
 
     ## iterative scaling
     expectation <- function(par){
         n <- switch(par,
                     "alpha" = N,
-                    "beta" = D,
-                    "theta" = S)
+                    "beta" = D)
         res <- numeric(n)
 
         for (k in seq_len(S)){
@@ -87,35 +85,33 @@ PlackettLuce <- function(rankings, maxit = 100, trace = FALSE){
             nobj <- length(w)
             ## ties of order d
             y1 <- numeric(n)
+            z1 <- 0
             for (d in seq_len(min(D, nobj))){
                 i <- seq_len(d)
                 maxi <- rev(nobj - seq_len(d) + 1)
                 ## loop through all subsets of size d in set
                 if (par == "alpha")
                     y2 <- numeric(n)
-                else y2 <- 0
+                z2 <- 0
                 repeat{
                     x <- prod(alpha[w[i]])^(1/d)
                     ## add to sums for all objects in set
                     if (par == "alpha")
-                        y2[w[i]] <- y2[w[i]] + theta[k] * rep[k] * x/d
+                        y2[w[i]] <- y2[w[i]] + x
                     ## add to sum for current order
                     if (par == "beta")
-                        y2 <- y2 + theta[k] * rep[k] * x
+                        y1[d] <- y1[d] + x
                     ## add to sum for current set
-                    if (par == "theta") y2 <- y2 + x
+                    z2 <- z2 + x
                     if (i[1] == maxi[1]) break
                     id <- max(which(maxi - i > 0))
                     i[id:d] <- i[id] + seq_len(d - id + 1)
                 }
                 if (par == "alpha")
-                    y1 <- y1 + y2*delta[d]
-                if (par == "beta")
-                    y1[d] <- y2*delta[d]
-                if (par == "theta")
-                    y1[k] <- y1[k] + y2*delta[d]
+                    y1 <- y1 + delta[d]/d*y2
+                z1 <- z1 + delta[d]*z2
             }
-            res <- res + y1
+            res <- res + rep[k]*y1/z1
         }
         res
     }
@@ -124,12 +120,10 @@ PlackettLuce <- function(rankings, maxit = 100, trace = FALSE){
         ## update all alphas
         old <- alpha
         alpha <- alpha*A/expectation("alpha")
-        ## scale alphas to mean 1
-        alpha <- alpha/sum(alpha)#alpha/mean(alpha)
+        ## scale alphas to sum 1
+        alpha <- alpha/sum(alpha)
         ## update all deltas
-        if (D > 1) delta[-1] <- delta[-1] * B[-1]/expectation("beta")[-1]
-        ## update thetas again
-        theta <- 1/expectation("theta")
+        if (D > 1) delta[-1] <- B[-1]/expectation("beta")[-1]
         ## trace
         if (trace){
             message("iter ", iter)
@@ -138,7 +132,6 @@ PlackettLuce <- function(rankings, maxit = 100, trace = FALSE){
         if (isTRUE(all.equal(log(old), log(alpha)))) break
     }
 
-    list(coefficients = structure(c(alpha, delta[-1]),
-                                  eliminated = theta),
+    list(coefficients = c(alpha, delta[-1]),
          iter = iter)
 }
