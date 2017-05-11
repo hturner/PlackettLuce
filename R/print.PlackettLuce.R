24d4c83602c77a04850e368a237f4c328ddf91fd
Heather Turner <ht@heatherturner.net>
2017-05-11 17:23:27 +0100

    add print method

---
+++ R/print.PlackettLuce.R
@@ -0,0 +1,14 @@
+#' @importFrom stats coef
+print.PlackettLuce <- function (x,
+                                digits = max(3, getOption("digits") - 3),
+                                ...)
+{
+    cat("Call: ", deparse(x$call), "\n", sep = "", fill = TRUE)
+    if (length(coef(x))) {
+        cat("Coefficients:\n")
+        print.default(format(coef(x), digits = digits), print.gap = 2L,
+                      quote = FALSE)
+    }
+    else cat("No coefficients\n")
+    invisible(x)
+}
