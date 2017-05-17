4720158643d9db55bbc0d76b620f428c458968dc
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-05-17 02:17:19 +0100

    Added a what argument to coef

--- R/coef.R
+++ R/coef.R
@@ -1,11 +1,15 @@
-coef.PlackettLuce <- function(object, ref = 1, ...){
+coef.PlackettLuce <- function(object, ref = 1, parameters = "all", ...){
+  what <-  match.arg(what, c("ties", "abilities", "all"))
   coefs <- log(object $ coefficients)
   ncoefs <- length(coefs)
   nobj <- ncoefs - object $ maxTied + 1
-  object_names <- names(coefs)[1:nobj] 
+  object_names <- names(coefs)[1:nobj]
   if (ref %in% object_names) ref <- which(object_names == ref)
   if (ref %in% 1:nobj) {
     coefs[1:nobj] <- coefs[1:nobj] - coefs[ref]
   } else stop("Invalid value for the 'ref' argument")
-  return(coefs)
+  switch(what,
+         "ties" = return(coefs[-c(1:nobj)]),
+         "abilities" = return(coefs[1:nobj]),
+         "all" = return(coefs))
 }
