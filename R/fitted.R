7b4eacb5cc6187e57fcd5a2eb2f1601d8408cd17
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-05-28 22:31:06 +0100

    Fitted probs from PlackettLuce objects

---
+++ R/fitted.R
@@ -0,0 +1,34 @@
+g## Todo:
+## Drop dependence on tibble
+## Avoid re-evaluation for repeated choices-alternatives combinations
+## Optimize subsetting in prob
+
+#' Fitted probabilities for PlackettLuce objects
+#'
+#' Fitted probabilities for all choices/alternatives combinations in the data
+#'
+#' @seealso as.choices
+#' @export
+fitted.PlackettLuce <- function(object, ...) {
+    patterns <- object$patterns
+    choices <- as.choices(object$rankings, name = FALSE)
+    choices <- tibble::as.tibble(choices)
+    objects <- attr(choices, "objects")
+    choices$D <- unlist(lapply(choices[["choices"]], length))
+    choices$T <- unlist(lapply(choices[["alternatives"]], length))
+    npatterns <- ncol(patterns)
+    nobjects <- nrow(patterns)
+    alpha <- object$coefficients[seq_len(nobjects)]
+    delta <- c(1, object$coefficients[-seq_len(nobjects)])
+    nconst <- object$constants
+    prob <- function(obs) {
+        D <- obs$D
+        alt <- obs$alternatives
+        ch <- obs$choices
+        if (obs$T == 1) 1 else { delta[D] * prod(alpha[ch])^(1/D) / nconst[colSums(patterns == (seq.int(nobjects) %in% alt)) == nobjects] }
+    }
+    choices$fitted <- apply(choices, 1, prob)
+    choices$D <- choices$T <- NULL
+    attr(choices, "objects") <- objects
+    choices
+}
