515dbcadbe17e30a6f9a9b6c340fce636f09eb95
Heather Turner <ht@heatherturner.net>
2017-07-17 11:18:53 +0100

    add plfit based on grouped_rankings
    doesn't work with pltree, can't have list column in model frame

---
+++ R/PLtree.R
@@ -0,0 +1,48 @@
+# 1. create a fit function as required by psychotools::mob
+plfit <- function (y, x = NULL, start = NULL, weights = NULL, offset = NULL,
+                   cluster = NULL, ..., estfun = FALSE, object = FALSE) {
+    x <- !(is.null(x) || NCOL(x) == 0L)
+    weights <- !is.null(weights)
+    offset <- !is.null(offset)
+    if (x || weights || offset)
+        warning("unused argument(s): ",
+                paste(c("x"[x], "weights"[weights], "offset"[offset]),
+                      collapse = ","))
+    # y is grouped_rankings object
+    R <- as.rankings(do.call("rbind", y), verbose = FALSE)
+    # return null result if network not strongly connected
+    if (attr(R, "no") > 1){
+        return(list(coefficients = NA, objfun = Inf,
+                    estfun = NULL, object = NULL))
+    }
+    res <- PlackettLuce(R, ...)
+    # returns with rownames - possible to avoid?
+    if (estfun) {
+        percomp <- estfun.PlackettLuce(res)
+        estfun <- rowsum(as.matrix(percomp),
+                         rep(seq_len(length(y)), sapply(y, nrow)))
+    } else estfun <- NULL
+    list(coefficients = coef(res), objfun = -res$loglik,
+         estfun = estfun,
+         object = if (object) res else NULL)
+}
+
+# 2. create a pltree function
+pltree <- function (formula, data, subset, na.action, cluster, ref = NULL, ...)
+{
+    m <- match.call(expand.dots = TRUE)
+    control_args <- names(m) %in% names(formals(mob_control))
+    # treating rankings response as data.frame
+    # => index correctly by mob; else need to add indexing method to treat as vector
+    args <- as.list(m)[control_args]
+    args$ytype <- "vector"
+    control <- do.call("mob_control", args)
+    m <- m[!control_args]
+    m$control <- control
+    m$fit <- plfit
+    m[[1L]] <- as.name("mob")
+    rval <- eval(m, parent.frame())
+    rval$info$call <- m
+    class(rval) <- c("pltree", class(rval))
+    return(rval)
+}
---
+++ R/grouped_rankings.R
@@ -0,0 +1,60 @@
+# Currently just conversion from paircomp object
+#' @rdname grouped_rankings
+#' @export
+as.grouped_rankings <- function(x, verbose = TRUE, ...){
+    UseMethod("as.grouped_rankings")
+}
+
+#' @rdname grouped_rankings
+#' @method as.grouped_rankings paircomp
+#' @export
+as.grouped_rankings.paircomp <- function(x, verbose = TRUE, ...){
+    if (attr(x, "mscale")[1] < -1) {
+        warning("strength of preference ignored")
+        x <- sign(x)
+    }
+    id <- which(!is.na(as.matrix(x)), arr.ind = TRUE)
+    ncomp <- nrow(id)
+    nobj <- length(attr(x, "labels"))
+    pairs <- which(upper.tri(diag(nobj)), arr.ind = TRUE)
+    rankings <- matrix(0, nrow = ncomp, ncol = nobj,
+                       dimnames = list(NULL, attr(x, "labels")))
+    x <- as.matrix(x)[id]
+    rankings[cbind(seq_len(ncomp), pairs[,1][id[,2]])] <- ifelse(x == -1, 2, 1)
+    rankings[cbind(seq_len(ncomp), pairs[,2][id[,2]])] <- ifelse(x == 1, 2, 1)
+    rankings <- as.rankings.matrix(rankings, verbose = verbose)
+    attr(rankings, "index") <- id[,1]
+    id  <- split(seq_len(ncomp), id[,1])
+    submatrix  <- function(M, i) M[i, , drop = FALSE]
+    rankings  <- lapply(id, submatrix, M = rankings)
+    class(rankings) <- "grouped_rankings"
+    rankings
+}
+
+# print method would be nice to separate rankings but ok
+#' @method as.data.frame grouped_rankings
+#' @export
+as.data.frame.grouped_rankings <-
+    function(x, row.names = NULL, optional = FALSE, ...,
+             nm = paste(deparse(substitute(x), width.cutoff = 20L), collapse = " ")){
+    value <- list(x)
+    if (!optional) {
+        names(value) <- nm
+    } else names(value) <- make.names(nm)
+    if (is.null(row.names) & !is.null(rownames(x))) row.names <- rownames(x)
+    if (is.null(row.names)) {
+        row.names <- .set_row_names(length(x))
+    } else {
+        if (is.object(row.names) || !is.integer(row.names))
+            row.names <- as.character(row.names)
+        if (anyNA(row.names))
+            stop("row names contain missing values")
+        if (anyDuplicated(row.names))
+            stop(paste("duplicate row.names: ",
+                       paste(unique(row.names[duplicated(row.names)]),
+                             collapse = ", ")))
+    }
+    attr(value, "row.names") <- row.names
+    class(value) <- "data.frame"
+    value
+}
