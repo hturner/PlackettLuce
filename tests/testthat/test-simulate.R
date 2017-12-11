369f9fb77e4439224123e0e06b0d9c30eba14b5a
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-12-11 02:00:28 +0000

    comment out simulation study in test-simulate.R

--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -18,18 +18,17 @@ test_that("the seed argument is respected in [simulate.PlackettLuce]", {
 })
 
 
-
-R <- PlackettLuce:::generate_rankings(maxi = 5, n_rankings = 200, tie = 0, seed = 123)
-mod1 <- PlackettLuce(R)
-samples <- simulate(mod1, 100, set.seed = 123)
-fits <- lapply(samples, PlackettLuce, npseudo = 0)
-coefs <- sapply(fits, function(fit) {
-    cc <- coef(fit)
-    if (length(cc) < 9)
-        c(cc, rep(0, 9 - length(cc)))
-    else
-        cc
-})
-
-par(mfrow = c(3, 3))
-for (j in 1:9) { hist(coefs[j,], main = paste(j)); abline(v = coef(mod1)[j]) }
+## A small simulation study
+## R <- PlackettLuce:::generate_rankings(maxi = 5, n_rankings = 200, tie = 0, seed = 123)
+## mod1 <- PlackettLuce(R)
+## samples <- simulate(mod1, 100, set.seed = 123)
+## fits <- lapply(samples, PlackettLuce, npseudo = 1/2)
+## coefs <- sapply(fits, function(fit) {
+##     cc <- coef(fit)
+##     if (length(cc) < 9)
+##         c(cc, rep(0, 9 - length(cc)))
+##     else
+##         cc
+## })
+## par(mfrow = c(3, 3))
+## for (j in 1:9) { hist(coefs[j,], main = paste(j)); abline(v = coef(mod1)[j]) }
