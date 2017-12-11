f73dab1be3305bb8753d8379b81dc35f1b2744ff
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2017-12-11 01:51:45 +0000

    Docs, notes and tests for simulate method

--- R/simulate.R
+++ R/simulate.R
@@ -1,67 +1,97 @@
-## TODO Add handling of grouped_rankings
-
-#' Simulate from PlackettLuce objects
+#' Simulate from \code{PlackettLuce} fitted objects
 #'
+#' @inheritParams stats::simulate
 #'
+#' @examples
+#' R <- matrix(c(1, 2, 0, 0,
+#'               4, 1, 2, 3,
+#'               2, 1, 1, 1,
+#'               1, 2, 3, 0,
+#'               2, 1, 1, 0,
+#'               1, 0, 3, 2), nrow = 6, byrow = TRUE)
+#' colnames(R) <- c("apple", "banana", "orange", "pear")
+#' mod <- PlackettLuce(R)
+#' simulate(mod, 5)
 #'
+#' s1 <- simulate(mod, 3, seed = 112)
+#' s2 <- simulate(mod, 2, seed = 112)
+#'
+#' identical(s1[1:2], s2[1:2])
+#'
+#' @method simulate PlackettLuce
+#' @export
 simulate.PlackettLuce <- function(object, nsim = 1, seed = NULL, ...) {
-        if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
-            runif(1)
-        if (is.null(seed))
-            RNGstate <- get(".Random.seed", envir = .GlobalEnv)
-        else {
-            R.seed <- get(".Random.seed", envir = .GlobalEnv)
-            set.seed(seed)
-            RNGstate <- structure(seed, kind = as.list(RNGkind()))
-            on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
+        runif(1)
+    if (is.null(seed))
+        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+    else {
+        R.seed <- get(".Random.seed", envir = .GlobalEnv)
+        set.seed(seed)
+        RNGstate <- structure(seed, kind = as.list(RNGkind()))
+        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+    }
+    rankings <- unclass(object$rankings)
+    N <- ncol(rankings)
+    n_rankings <- nrow(rankings)
+    id <- seq(length(object$coefficients) - object$maxTied + 1)
+    alpha <- object$coefficients[id]
+    delta <- numeric(N)
+    delta[seq_len(object$maxTied)] <- c(1, unname(object$coefficients[-id]))
+    opt <- seq_len(N)
+    sets <- as.list(numeric(n_rankings))
+    for (i in seq_len(n_rankings)) {
+        sets[[i]] <- opt[rankings[i,] > 0]
+    }
+    len <- lengths(sets)
+
+    ## NOTE, IK 10/12/2017: This is an awful computation with potentially massive objects
+    ## FIX, IK 10/12/2017: Remove dependence on all combinations. For now
+    ## a preventive clause if categories are more than 10
+    if (any(len > (max_items <- 10))) {
+        stop(paste("detected more than", max_items, "items per ranking; current implementation of simulate.PlackettLuce is not apropriate for large number of items"))
+    }
+    ## Get all possible combinations of objects
+    combinations <- NULL
+    for (j in seq_len(max(len))) {
+        combinations <- c(combinations, combn(opt, j, simplify = FALSE))
+    }
+
+    ## Unormalized probabilities
+    probs <- sapply(combinations, function(z) delta[length(z)] * prod(alpha[z])^(1/j))
+    n_choices <- length(probs)
+
+    ## NOTE, IK 10/12/2017: Normalization will be done internally by sample.int
+    sampler <- function(objects) {
+        v <- numeric(N)
+        j <- 1
+        while (length(objects)) {
+            ## find out which combinations have all of their objects included in `objects`
+            indices <- sapply(combinations, function(x) {
+                all(x %in% objects)
+            })
+            ## sample, after setting the probability of all other combinations to zero
+            sampled <- combinations[[sample.int(n_choices, 1, prob = probs * indices)]]
+            ## remove the sampled objects from `objects`
+            objects <- objects[-match(sampled, objects, nomatch = 0)]
+            v[sampled] <- j
+            j <- j + 1
         }
+        v
+    }
 
-        rankings <- unclass(object$rankings)
-        N <- ncol(rankings)
-        id <- seq(length(object$coefficients) - object$maxTied + 1)
-        alpha <- object$coefficients[id]
-        delta <- numeric(N)
-        delta[seq_len(object$maxTied)] <- c(1, unname(object$coefficients[-id]))
-
-        opt <- seq_len(N)
-        sets <- apply(rankings, 1, function(x) opt[x > 0])
-##:ess-bp-start::browser@nil:##
-browser(expr=is.null(.ESSBP.[["@15@"]]));##:ess-bp-end:##
-
-        ## Get all possible combinations of objects
-        ## NOTE, IK 10/12/2017: This is an awful computation with many objects
-        combinations <- NULL
-        for (j in opt) {
-            combinations <- c(combinations, combn(opt, j, simplify = FALSE))
-        }
-        ## Unormalized probabilities
-        probs <- sapply(combinations, function(z) delta[length(z)] * prod(alpha[z])^(1/j))
-        n_choices <- length(probs)
-
-        ## NOTE, IK 10/12/2017: Normalization will be done internally by sample.int
-        ## NOTE, IK 10/12/2017: Can be written as recursion but while neat it will be slower
-        sampler <- function(objects) {
-            v <- numeric(N)
-            j <- 1
-            while (length(objects)) {
-                indices <- sapply(combinations, function(x) {
-                    all(x %in% objects)
-                })
-                sampled <- combinations[[sample.int(n_choices, 1, prob = probs * indices)]]
-                objects <- objects[-match(sampled, objects, nomatch = 0)]
-                v[sampled] <- j
-                j <- j + 1
-            }
-            v
-        }
-
-        out <- replicate(nsim, {
-            R <- t(sapply(sets, sampler))
-            colnames(R) <- colnames(rankings)
-            as.rankings(R)
-        }, simplify = FALSE)
-        names(out) <- paste("sim", seq_len(nsim), sep = "_")
-        out
+    out <- replicate(nsim, {
+        R <- t(sapply(sets, sampler))
+        colnames(R) <- colnames(rankings)
+        as.rankings(R)
+    }, simplify = FALSE)
+    names(out) <- paste("sim", seq_len(nsim), sep = "_")
+    out
 }
 
 
+## TODO, IK, 10/12/2017:
+## * Handling of weights
+## * Avoid constructing all combinations
+## * Exploit ideas for multinomial sampling with massive number of categories
+## * Add parallelization capabilities
---
+++ tests/testthat/test-simulate.R
@@ -0,0 +1,35 @@
+context("implementation [simulate.PlackettLuce]")
+
+
+R <- matrix(c(1, 2, 0, 0,
+              4, 1, 2, 3,
+              2, 1, 1, 1,
+              1, 2, 3, 0,
+              2, 1, 1, 0,
+              1, 0, 3, 2), nrow = 6, byrow = TRUE)
+colnames(R) <- c("apple", "banana", "orange", "pear")
+mod <- PlackettLuce(R)
+simulate(mod, 5)
+s1 <- simulate(mod, 5, seed = 112)
+s2 <- simulate(mod, 4, seed = 112)
+
+test_that("the seed argument is respected in [simulate.PlackettLuce]", {
+    expect_identical(s1[1:3], s2[1:3])
+})
+
+
+
+R <- PlackettLuce:::generate_rankings(maxi = 5, n_rankings = 200, tie = 0, seed = 123)
+mod1 <- PlackettLuce(R)
+samples <- simulate(mod1, 100, set.seed = 123)
+fits <- lapply(samples, PlackettLuce, npseudo = 0)
+coefs <- sapply(fits, function(fit) {
+    cc <- coef(fit)
+    if (length(cc) < 9)
+        c(cc, rep(0, 9 - length(cc)))
+    else
+        cc
+})
+
+par(mfrow = c(3, 3))
+for (j in 1:9) { hist(coefs[j,], main = paste(j)); abline(v = coef(mod1)[j]) }
