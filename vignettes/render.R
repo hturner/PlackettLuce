196c97f3901097db026d74f86a8aa81d94a30ebc
Heather Turner <ht@heatherturner.net>
2020-10-06 13:32:35 +0100

    use bookdown::html_document2 for pkgdown

--- vignettes/render.R
+++ vignettes/render.R
@@ -2,33 +2,17 @@
 rmarkdown::render("vignettes/Overview.Rmd", output_dir = "vignettes_tmp_output",
                   clean = FALSE)
 
+rmarkdown::render("vignettes/Overview.Rmd", "bookdown::html_document2",
+                  output_dir = "vignettes_tmp_output",
+                  clean = FALSE)
+
+## ! LaTeX Error: Environment cslreferences undefined.
 rmarkdown::render("vignettes/Overview.Rmd", "BiocStyle::pdf_document",
-                  output_dir = "vignettes_tmp_output")
+                  output_dir = "vignettes_tmp_output",
+                  clean = FALSE)
 
 library(devtools)
 library(pkgdown)
-# no longer works, just hack current pkgdown function
-#source_gist("https://gist.github.com/hturner/3152081e223ade0bb212bcef19f183bf",
-#            filename = "build_rmarkdown_format.R")
-
-assignInNamespace(
-    "build_rmarkdown_format",
-    ns = "pkgdown",
-    value = function(pkg = ".",
-                     name,
-                     depth = 1L,
-                     data = list(),
-                     toc = TRUE) {
-        template <- pkgdown:::rmarkdown_template(pkg, name, depth = depth,
-                                       data = data)
-        out <- bookdown::html_document2(toc = toc, toc_depth = 2,
-                                        self_contained = FALSE, theme = NULL,
-                                        template = template$path)
-        out$knitr$opts_chunk <- pkgdown:::fig_opts_chunk(pkg$figures, out$knitr$opts_chunk)
-        attr(out, "__cleanup") <- template$cleanup
-        out
-    }
-)
 
 # either a) rebuild whole pkgdown site including help files
 build_site(lazy = FALSE)
